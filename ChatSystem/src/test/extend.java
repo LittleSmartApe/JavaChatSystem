1931b82415f0bf57cfbb4dbf09ce448a489eaacc
cheng <cheng@DESKTOP-1L68BJH>
2020-05-09 20:59:33 +0800

    1

---
+++ ChatSystem/src/test/extend.java
@@ -0,0 +1,47 @@
+package test;
+import java.util.*;
+public class extend {
+	public static void main(String args[]) {
+		Student xm = new Student();
+		xm.id = 888;
+		System.out.println(String.format("%.0f", xm.id));
+		xm.Fuck();
+	}
+}
+abstract class People{
+	protected float id;
+	protected float highth;
+	
+}
+
+
+interface Do{
+	void Fuck();
+	void Drink();
+	void Pee();
+}
+
+class Student extends People implements Do{
+	float getPeopleId(){
+		return super.id;
+	}
+
+	@Override
+	public void Fuck() {
+		System.out.println("You have fucked a People and her got pregnanted ");
+	}
+
+	@Override
+	public void Drink() {
+		
+	}
+
+	@Override
+	public void Pee() {
+		
+	}
+}
+
+class Man extends People{
+	
+}

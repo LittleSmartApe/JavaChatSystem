0a3527073bad37378b5882e31fbc7adb7096524e
lewhuson <lewhuson@LAPTOP-TJMDTAFG>
2020-05-09 21:50:57 +0800

    3

--- ChatSystem/src/test/extend.java
+++ ChatSystem/src/test/extend.java
@@ -28,7 +28,7 @@ class Student extends People implements Do{
 
 	@Override
 	public void Fuck() {
-		System.out.println("You have fucked a Person haha");
+		System.out.println("You have fucked a Person hahddda");
 	}
 
 	@Override

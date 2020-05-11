57a92d763d7882cbe0f79ba4ec978f23455b6007
cheng <cheng@DESKTOP-1L68BJH>
2020-05-11 18:07:12 +0800

    自定义异常尝试

--- ChatSystem/src/test/extend.java
+++ ChatSystem/src/test/extend.java
@@ -1,11 +1,27 @@
 package test;
+
 import java.util.*;
 public class extend {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws PeeException {
 		Student xm = new Student();
 		xm.id = 888;
 		System.out.println(String.format("%.0f", xm.id));
 		xm.Fuck();
+		try {
+			xm.Fuck();
+		}catch(Exception e){
+			System.out.println("There is nothing to fuck!");
+		}finally {
+			System.out.println("Go fuck your self!");
+		}
+		try {
+			xm.Pee(0);
+		}catch(PeeException e){
+			System.out.println(e.getMessage());
+		}
+		xm.Pee(1);
+		xm.Pee(0);
+		
 	}
 }
 abstract class People{
@@ -18,39 +34,41 @@ abstract class People{
 interface Do{
 	void Fuck();
 	void Drink();
-	void Pee();
+	void Pee(int a) throws PeeException;
 }
 
 class Student extends People implements Do{
 	float getPeopleId(){
 		return super.id;
-		dsafsdfds'afsdk'afas'kfas
 	}
 
 	@Override
 	public void Fuck() {
-<<<<<<< HEAD
-		fucksdalfjdssdafdsf
-		System.out.println("You have fucked a Person  576565");
-=======
-		System.out.println("You have fucked a Person 023132");
->>>>>>> branch 'master' of https://github.com/LittleSmartApe/JavaChatSystem.git
+		System.out.println("You have fucked a People and her got pregnanted ");
 	}
 
 	@Override
 	public void Drink() {
 		
 	}
-
 	@Override
-	public void Pee() {
+	public void Pee(int a) throws PeeException {
+		if (a == 1) System.out.println("You pee!");
+		else throw new PeeException("You can't Pee!");	
 		
 	}
-}
 
-class Man extends People{
-	
 }
-class women extends People{
+class PeeException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PeeException(String a) {
+		super(a);
+	}
+}
+class Man extends People{
 	
 }
